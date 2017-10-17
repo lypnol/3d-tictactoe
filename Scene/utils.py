ad866a51bd90dc35fc2be3b8b1e77c2828f6bcfa
Charif Chaibainou <chaibainoucharif@gmail.com>
2017-10-17 16:24:52 +0200

    working version of the game startegy

--- Scene/utils.py
+++ Scene/utils.py
@@ -4,19 +4,34 @@ def hex_to_color_vector(h):
      return vector(*tuple(int(h[i:i+2], 16) / 256 for i in (0, 2 ,4)))
 
 def getNeighbor(grid, startx, starty, startz):
-    return([grid[startx+i][starty+j][startyz+k] for i in [-1,0,1] for j in [-1,0,1] for k in [-1,0,1]])
-#ne pas oublier d'exclure les cas où le point sort du grid (hors de 0,1,2)
+    neighbors= list()
+    for i in [-1,0,1]:
+        for j in [-1,0,1]:
+            for k in [-1,0,1]:
+                if i == j == k == 0:
+                    continue
+                elif startx+i in [0,1,2] and starty+j in [0,1,2] and startz+k in [0,1,2]:
+                    neighbors.append((grid[startx+i][starty+j][startz+k],[startx+i,starty+j,startz+k]))
+    return neighbors
 
 def getVector(firstPoint, secondPoint):
     return ([firstPoint[0]-secondPoint[0], firstPoint[1]-secondPoint[1], firstPoint[2]-secondPoint[2]])
 
 def isParallel(firstVector, secondVector):
-    quotient = []
-    remainder = []
-    for i in range(2):
-        quotient[i] = firstVector[i] // secondVector[i]
-        remainder[i] = firstVector[i] % secondVector[i]
-    for item in remainder:
-        if item != 0:
-            return False
-    return quotient[1:] == quotient[:-1]
+    coef = None
+    verifier = True
+    for i in [0,1,2]:
+        if firstVector[i] == 0:
+            if secondVector[i] != 0:
+                verifier = False
+                break
+            continue
+        elif coef is None:
+            coef = secondVector[i]/firstVector[i]
+        if coef==0:
+            verifier = False
+            break
+        elif coef != secondVector[i]/firstVector[i]:
+            verifier = False
+            break
+    return verifier
--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,5 @@
 import itertools
-
+from Scene.utils import getNeighbor, isParallel, getVector
 
 class TicTacToe:
     def __init__(self, game_scene, n, current_player='x'):
@@ -14,44 +14,19 @@ class TicTacToe:
         for x in range(n):
             for y in range(n):
                 for z in range(n):
-                    winnerList = [g[x][y][z]]
-                    #contient l'éventuel triplet gagnant
-                    if g[x][y][z] == 'x':
-                        for item in getNeighbor(g,x,y,z):
-                            if item == 'x':
-                                winnerList.append(item)
-                            #on remplit la liste de tous les voisins immédiats de g de meme couleur
-                        if len(winnerList)>1:
-                            for item2 in winnerList[1:]:
-                                lastNeighbors = getNeighbor(item2, item2[0], item2[1], item2[2])
-                                #on regarde les voisins immédiats de chaque voisin trouvé précédemment
-                                for i in range(lastNeighbors):
-                                    if lastNeighbors[i] == 'x':
-                                        if isParallel(getVector(item2,g), getVector(item2,lastNeighbors[i])):
-                                            return('x', [g, item2, lastNeighbors[i]])
-                                     #ici, on détermine si le vecteur formé par item2 (voisins immédiats de g
-                                     #de meme couleur) et g est proportionnel
-                                     # à celui formé par item2 et l'un de ses voisins immédiat
-                                     #si c'est le cas, c'est gagné
-                        else:
-                            continue
-                    elif g[x][y][z] == 'o':
-                        for item in getNeighbor(g,x,y,z):
-                            if item == 'o':
-                                winnerList.append(item)
-                        if len(winnerList)>1:
-                            for item2 in winnerList[1:]:
-                                lastNeighbors = getNeighbor(item2, item2[0], item2[1], item2[2])
-                                for i in range(lastNeighbors):
-                                    if lastNeighbors[i] == 'o':
-                                        if isParallel(getVector(item2,g), getVector(item2,lastNeighbors[i])):
-                                            return('o', [g, item2, lastNeighbors[i]])
-                        else:
-                            continue
-                    elif g[x][y][z] == None:
-                        return None, None
-                    else:
-                        return 'NULL'
+                    neighbors = getNeighbor(g,x,y,z)
+                    #for item in neighbors:
+                    #neighbors[0] est le 1er voisin de la liste
+                    #neighbors[0][0] est la couleur
+                    while neighbors:
+                        if neighbors[0][0] == g[x][y][z]:
+                            for otherNeighbor in neighbors[1:]:
+                                if otherNeighbor[0] == g[x][y][z] \
+                                and isParallel(getVector(neighbors[0][1],(x,y,z)), getVector(otherNeighbor[1],(x,y,z))):
+                                    return ("It's a win", [x,y,z], neighbors[0][1], otherNeighbor[1])
+                            neighbors.pop(0)
+        return None
+
 
         # TODO Logic du jeu 3d
         # On doit utiliser l'état du jeu stocké dans la matrice 3x3x3 g
