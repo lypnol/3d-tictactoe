b4eaeff9886313a87ca4d3102c3243822dd528bc
ayoub <ayoub.sbai@student.ecp.fr>
2017-10-17 16:30:46 +0200

    added stuff

--- Scene/GameScene.py
+++ Scene/GameScene.py
@@ -21,11 +21,16 @@ class GameScene(BaseScene):
         self.disable_actions = False
         # objets
         self.boxes = {}
+        self.restart_button_local = None
+        self.restart_button_remote = None
+        self.restart_label_local = None
+        self.restart_label_remote = None
         self.title = None
         self.selected = set()
         self.curve = None
         # external events handlers
         self._on_select = None
+        self._on_restart = None
 
     def box_id_to_pos(self, box_id):
         n = self.n
@@ -34,11 +39,14 @@ class GameScene(BaseScene):
         return vector(x, y, z)
 
     def wait_for_opponent(self):
+        self.hide_restart()
         self.disable_actions = True
+        self.title.color = color.white
         self.title.text = 'Waiting for opponent...'
         self.title.visible = True
 
     def component_is_ready(self):
+        self.hide_restart()
         self.disable_actions = False
         self.title.text = 'Your turn'
         self.title.color = self.x_color
@@ -47,21 +55,55 @@ class GameScene(BaseScene):
     def init_objects(self):
         n = self.n
         self.title = label(pos=vector(0, 20, 0), xoffset=0, yoffset=0, text='Waiting for opponent...', align='center', color=color.white, opacity=0, line=False, height=25, box=False)
+        self.restart_button_local = box(pos=vector(-8, -15, 0), size=vector(15, 4, 2))
+        self.restart_button_remote = box(pos=vector(8, -15, 0), size=vector(15, 4, 2))
+        self.restart_label_local = label(pos=self.restart_button_local.pos, xoffset=0, yoffset=0, text='Restart Local', color=color.black, opacity=0, line=False, height=20, box=False)
+        self.restart_label_remote = label(pos=self.restart_button_remote.pos, xoffset=0, yoffset=0, text='Restart Remote', color=color.black, opacity=0, line=False, height=20, box=False)
         for box_id in itertools.product([i for i in range(n)], repeat=3):
             self.boxes[box_id] = box(
                 pos=self.box_id_to_pos(box_id),
                 color=color.white,
                 length=self.BOX_SIZE, height=self.BOX_SIZE, width=self.BOX_SIZE)
-        return [self.title] + list(self.boxes.values())
+        return [self.title, self.restart_button_local, self.restart_button_remote, self.restart_label_local, self.restart_label_remote] + list(self.boxes.values())
+
+    def restart(self, game_type='local'):
+        self.selected = set()
+        for box_id, box in self.boxes.items():
+            box.color = color.white
+        if self.on_restart: self.on_restart(game_type)
 
     def draw_link(self, link):
         if self.curve is None:
             self.curve = curve([self.boxes[box_id].pos for box_id in link], color=self.current_color)
 
     def game_over(self, end_data):
-        (result, points) = end_data
+        result, points = end_data
+        if result == 'NULL':
+            self.title.text = 'Draw'
+            self.title.color = white.color
+        else:
+            self.title.text = 'Player wins'
         # TODO Ce qu'on fait quand la partie est terminée
         # end_data: est ce qui est retourné par TicTacToe.check_end_game quand result est different de None
+        self.show_restart()
+
+    def show_restart(self):
+        self.restart_button_local.visible = True
+        self.restart_button_remote.visible = True
+        self.restart_label_local.visible = True
+        self.restart_label_remote.visible = True
+
+    def hide_restart(self):
+        self.restart_button_local.visible = False
+        self.restart_button_remote.visible = False
+        self.restart_label_local.visible = False
+        self.restart_label_remote.visible = False
+
+    def on_opponent_left(self):
+        self.title.color = color.red
+        self.title.text = 'Opponent left'
+        self.disable_actions = True
+        self.show_restart()
 
     def switch_colors(self):
         if self.current_color == self.o_color:
@@ -81,6 +123,11 @@ class GameScene(BaseScene):
                     self.selected.add(box_id)
                     if self.on_select is not None: self.on_select(box_id)
                     break
+        else:
+            if obj == self.restart_button_local:
+                self.restart('local')
+            elif obj == self.restart_button_remote:
+                self.restart('remote')
 
     def select_box(self, box_id):
         x, y, z = box_id
--- Server.py
+++ Server.py
@@ -47,32 +47,47 @@ class ConnectionHandler(Thread):
         return data
 
     def start_game(self, player_symbol, opponent, game):
-        self.conn.send(struct.pack(DATA_FORMAT, b's', 0 if player_symbol == 'o' else 1, 0, 0))
+        self.game = game
         self.player_symbol = player_symbol
         self.opponent = opponent
-        self.game = game
+        self.conn.send(struct.pack(DATA_FORMAT, b's', 0 if player_symbol == 'o' else 1, 0, 0))
 
     def send_move(self, data):
         self.conn.send(struct.pack(DATA_FORMAT, b'm', *data))
 
     def end_game(self, data):
-        self.conn.send(struct.pack(DATA_FORMAT, b'e', *data))
+        self.game = None
+        self.opponent = None
 
     def run(self):
-        while not self._closed:
-            data = self.read_bytes(struct.calcsize(DATA_FORMAT))
-            parts = struct.unpack(DATA_FORMAT, data)
-            cmd, data = parts[0], parts[1:]
-            if cmd == b's':
-                self.server.game_request(self)
-            elif cmd == b'm' and self.opponent and self.game.current_player == self.player_symbol:
-                self.game.on_select(data)
-                concurrent_print("Player %s move %s" % (self, data))
-                self.opponent.send_move(data)
+        try:
+            while not self._closed:
+                data = self.read_bytes(struct.calcsize(DATA_FORMAT))
+                parts = struct.unpack(DATA_FORMAT, data)
+                cmd, data = parts[0], parts[1:]
+                if cmd == b's':
+                    self.server.game_request(self)
+                elif cmd == b'm' and self.opponent and self.game.current_player == self.player_symbol:
+                    concurrent_print("Player %s move %s" % (self, data))
+                    match_ended = self.game.on_select(data, debug=False)
+                    self.opponent.send_move(data)
+                    if match_ended:
+                        self.end_game()
+        except:
+            self.close()
+
+    def in_game(self):
+        return not self._closed and self.game is not None and self.opponent is not None
 
     def close(self):
         self._closed = True
-        self.conn.close()
+        if self.conn:
+            self.conn.close()
+        if self.in_game():
+            self.opponent.end_game()
+            self.opponent.conn.send(struct.pack(DATA_FORMAT, b'e', 0, 0, 0))
+        if self.server:
+            self.server.disconnect(self)
 
 class Queue:
     def __init__(self):
@@ -86,6 +101,12 @@ class Queue:
                 return (self.queue.pop(), self.queue.pop())
             return None, None
 
+    def remove(self, e):
+        with self.lock:
+            try:
+                self.queue.remove(e)
+            except ValueError: pass
+
 class Server:
     def __init__(self, host=HOST, port=PORT, max_connections=MAX_CONNECTIONS):
         self.port = port
@@ -94,22 +115,18 @@ class Server:
         self.conn_identifier = 0
         self.id_lock = RLock()
         self.game_requests = Queue()
-        self.games = {}
-        self.games_lock = RLock()
 
     def game_request(self, player):
         concurrent_print("Received game request from %s" % (player))
         player1, player2 = self.game_requests.pushpop2(player)
-        if player1 and player2:
-            with self.games_lock:
-                games = self.games
-                if player1 not in games and player2 not in games:
-                    game = TicTacToe(N)
-                    self.games[player1] = game
-                    self.games[player2] = game
-                    player2.start_game('o', player1, game)
-                    player1.start_game('x', player2, game)
-                    concurrent_print("Started game between %s and %s" % (player1, player2))
+        if player1 and player2 and not player1.in_game() and not player2.in_game():
+            game = TicTacToe(N)
+            player2.start_game('o', player1, game)
+            player1.start_game('x', player2, game)
+            concurrent_print("Started game between %s and %s" % (player1, player2))
+
+    def disconnect(self, player):
+        self.game_requests.remove(player)
 
     def start(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_server:
