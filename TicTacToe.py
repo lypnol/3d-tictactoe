347107948208f48b33d09efd90efdcb8777f4efc
ayoub <ayoub.sbai@student.ecp.fr>
2017-11-06 15:52:10 +0100

    Stable version

--- TicTacToe.py
+++ TicTacToe.py
@@ -23,11 +23,12 @@ class TicTacToe:
         self.grid = [[[None for z in range(self.n)] for y in range(self.n)] for x in range(self.n)]
         self.game_type = game_type
         if self.game_type == 'remote' and self.game_scene and self.remote_socket:
-            self.game_scene.wait_for_opponent()
+            self.game_scene.searching_for_opponent()
             self.remote_socket.find_game()
         elif self.game_type == 'local' and self.game_scene:
-            self.game_scene.opponent_is_ready()
-        self.current_player = 'x'
+            self.current_player = 'x'
+            self.game_scene.start_new_game()
+            self.game_scene.player_turn(self.current_player)
 
     def check_end_game(self):
         """returns the id of the winning cubes and their color"""
@@ -68,32 +69,31 @@ class TicTacToe:
             self.current_player = 'o'
         else:
             self.current_player = 'x'
-        if self.game_scene:
-            self.game_scene.switch_colors()
 
     def _apply_move(self, box_id):
         x, y, z = box_id
         self.grid[x][y][z] = self.current_player
+        if self.game_scene:
+            self.game_scene.select_box(box_id, self.current_player)
         match_ended = self.check_end_game()
         if match_ended is not None: return match_ended
         self.switch_player()
         return None
 
-    def on_start_game(self, current_player):
-        print("Started game as {}".format(current_player))
-        self.current_player = current_player
+    def on_start_game(self, as_symbol):
+        print("Started game as {}".format(as_symbol))
+        self.current_player = 'x'
         if self.game_scene:
-            self.game_scene.opponent_is_ready()
-            if self.current_player == 'o':
-                self.game_scene.disable_actions = True
-                self.game_scene.switch_colors()
+            self.game_scene.start_new_game()
+            if as_symbol == 'o':
+                self.game_scene.opponent_turn('x')
+            else:
+                self.game_scene.player_turn('x')
 
     def on_recv_move(self, box_id, debug=True):
         if debug: print("Player {} move {}".format(self.current_player, box_id))
-        if self.game_scene:
-            self.game_scene.select_box(box_id)
-            self.game_scene.disable_actions = False
         match_ended = self._apply_move(box_id)
+        self.game_scene.player_turn(self.current_player)
         if match_ended and self.game_scene:
             if debug: print("Match ended {} {}".format(*match_ended))
             self.game_scene.game_over(match_ended)
@@ -101,13 +101,18 @@ class TicTacToe:
     def on_select(self, box_id, debug=True):
         if debug: print("Player {} move {}".format(self.current_player, box_id))
         match_ended = self._apply_move(box_id)
-        if self.game_type == 'remote' and self.game_scene and self.remote_socket:
-            self.game_scene.disable_actions = True
+
+        if self.game_scene and self.game_type == 'remote':
+            self.game_scene.opponent_turn(self.current_player)
+        elif self.game_scene:
+            self.game_scene.player_turn(self.current_player)
+
+        if self.game_type == 'remote' and self.remote_socket:
             self.remote_socket.send_move(box_id)
+
         if self.game_scene and match_ended:
             if debug: print("Match ended {} {}".format(*match_ended))
             self.game_scene.game_over(match_ended)
-            self.game_scene.disable_actions = True
         return match_ended
 
     def on_opponent_left(self):
